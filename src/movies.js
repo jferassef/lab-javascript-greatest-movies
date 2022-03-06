2ee0fae5dc6732cb684fcf14ae36f11a93549148
Jessica Fernandez <jferasef@gmail.com>
2022-03-06 04:00:03 +0100

    Until excercise 3

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,66 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+function getAllDirectors(movies) {
+  let mappedArray = [];
+  let filteredArray = [];
+
+  movies.map((data1) => {
+    mappedArray.push(data1.director);
+  });
+
+  movies.filter((data1, data2) => {
+    if (data1.director != data2.director) {
+      filteredArray.push[data1];
+    }
+  });
+
+  return mappedArray;
+  return filteredArray;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  let spilbergFilms = [];
+
+  movies.map((data1) => {
+    if (data1.director == 'Steven Spielberg' && data1.genre.includes('Drama')) {
+      spilbergFilms.push(data1);
+    }
+  });
+
+  return spilbergFilms.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  let scoreData = 0;
+
+  if (movies.length == 0) {
+    return 0;
+  }
+
+  movies.map((data1) => {
+    if (data1.score != undefined) {
+      scoreData += data1.score;
+    }
+  });
+
+  let scoreAvg = scoreData / movies.length;
+  return Number(scoreAvg.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  let yearArr = [];
+  movies.sort((a, b) => {
+    return a.year - b.year;
+  });
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
@@ -23,8 +70,6 @@ function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -37,6 +82,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
